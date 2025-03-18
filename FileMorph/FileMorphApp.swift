261be6f1d62398e4fbd7398dbfe1067a6a6a7fe5
AniruddhaChattopadhyay <studyaniruddha@gmail.com>
2025-03-18 19:51:22 +0530

    Initial Commit

---
+++ FileMorph/FileMorphApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileMorphApp.swift
+//  FileMorph
+//
+//  Created by Aniruddha on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FileMorphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FileMorphTests/FileMorphTests.swift
@@ -0,0 +1,17 @@
+//
+//  FileMorphTests.swift
+//  FileMorphTests
+//
+//  Created by Aniruddha on 18/03/25.
+//
+
+import Testing
+@testable import FileMorph
+
+struct FileMorphTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
